66f7a589a724d344c6d5f96823747b29e58e3c7f
xjywq <3156095276@qq.com>
2021-10-20 17:28:14 +0800

    修改主页面, 报表

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,48 +1,47 @@
 <template>
 	<view>
 		<view class="uni-padding-wrap uni-common-mt">
-			<view class="uni-btn-v"><button type="primary" @click="openDB">打开数据库Mymoney.db</button></view>
+			<view class="uni-btn-v"><button type="primary" @click="OpenDB">打开数据库Mymoney.db</button></view>
 			<view class="uni-btn-v"><button type="primary" @click="selectSQL">查询表database的数据</button></view>
 			<view class="uni-btn-v"><button type="primary" @click="droptable">删除表database</button></view>
 			<view class="uni-btn-v"><button type="primary" @click="closeDB">关闭数据库Mymoney.db</button></view>
 			<button class="jump" @tap="navigateTo">上传</button>
 			<button class="jump" @tap="updateClick">刷新</button>
-			<!--<view>
-				<qiun-title-bar title="统计时间"/>
-				<picker mode="selector" @change="timeFrameChange" :range="timeArray" :value="0">
-					<view style="padding-left: 40rpx;"><span class="whatever-some-font">{{timeArray[timeIndex]}}</span></view>
-				</picker>
-			</view><br>
-			<qiun-title-bar title="支出情况"/>
-			<view class="charts-box" @longpress="outDetail" >
-				<qiun-data-charts type="ring" :opts="outOpts" :chartData="outData"/>
-			</view>
-			
-			<qiun-title-bar title="收入情况"/>
-			<view class="charts-box" @longpress="inDetail" >
-				<qiun-data-charts type="ring" :opts="inOpts" :chartData="inData"/>
-			</view> -->
+			<qiun-title-bar title="支出" />
+			<uni-table border stripe emptyText="无更多支出数据" >
+				<uni-tr v-for="record in sql_data" v-if="record.income=='false'">
+					<uni-td>{{record["tags"]}}</uni-td>
+					<uni-td>{{record["day"]}}</uni-td>
+					<uni-td>{{record["price"]}}</uni-td>
+					<uni-td>{{record["comment"]?record["comment"]:"无"}}</uni-td>
+				</uni-tr>
+			</uni-table><br>
+			<qiun-title-bar title="收入" />
+			<uni-table border stripe emptyText="无更多收入数据" >
+				<uni-tr v-for="record in sql_data" v-if="record.income=='true'">
+					<uni-td>{{record["tags"]}}</uni-td>
+					<uni-td>{{record["day"]}}</uni-td>
+					<uni-td>{{record["price"]}}</uni-td>
+					<uni-td>{{record["comment"]?record["comment"]:"无"}}</uni-td>
+				</uni-tr>
+			</uni-table><br>
 		</view>
 	</view>
 </template>
 
 <script>
 	import "../../common/basic_method.js";
+	import {openDB} from "../../common/DB_method.js";
 	export default {
 		data() {
-			this.openDB();
 			return {
-				outData:{},
-				outOpts: {},
-				inData:{},
-				inOpts: {},
-				timeArray: ["近一年", "近一个月", "近一周"],
 				timeIndex: 0,
 				title: 'SQLite',
 				sql_data: []
 			};
 		},
 		onLoad: function() {
+			openDB();
 			this.reload();
 		},
 		onShow: function() {
@@ -52,31 +51,12 @@
 			this.updateClick();
 		},
 		methods: {
-			timeFrameChange (e) {
+			timeFrameChange(e) {
 				this.timeIndex = e.target.value;
 				this.reload();
 			},
-			openDB() {
-				if (
-					plus.sqlite.isOpenDatabase({
-						name: 'moneymap',
-						path: '_doc/Mymoney.db'
-					})
-				) {
-					// plus.nativeUI.alert('Opened!');
-				} else {
-					// plus.nativeUI.alert('Unopened!');
-					plus.sqlite.openDatabase({
-						name: 'moneymap',
-						path: '_doc/Mymoney.db',
-						success: function(e) {
-							// plus.nativeUI.alert('打开数据库Mymoney.db成功 ');
-						},
-						fail: function(e) {
-							plus.nativeUI.alert('打开数据库Mymoney.db失败: ' + JSON.stringify(e));
-						}
-					});
-				}
+			OpenDB: function() {
+				openDB();
 			},
 			// 查询SQL语句
 			selectSQL: function() {
@@ -84,7 +64,7 @@
 					name: 'moneymap',
 					sql: 'select * from database',
 					success: function(e) {
-						plus.nativeUI.alert('查询SQL语句成功: ' + JSON.stringify(e));
+						// plus.nativeUI.alert('查询SQL语句成功: ' + JSON.stringify(e));
 					},
 					fail: function(e) {
 						plus.nativeUI.alert('查询SQL语句失败: ' + JSON.stringify(e));
@@ -127,70 +107,26 @@
 					title: '刷新成功',
 				});
 			},
-			
+
 			dic2list(dic) {
 				var ret = new Array();
 				for (var key in dic) {
-					ret.push({"name":key, "value": dic[key]});
+					ret.push({
+						"name": key,
+						"value": dic[key]
+					});
 				}
 				return ret;
 			},
-			
-			updateChart () {
-				var sql_data = this.sql_data;
-				var outcount = 0, incount = 0;
-				var outData = {}, inData = {};
-				sql_data.forEach( function(item) {
-					if (item["income"] == "true") {
-						incount += item["price"];
-						if (!inData.hasOwnProperty(item["tags"])) inData[item["tags"]] = item["price"];
-						else inData[item["tags"]] += item["price"];
-					}
-					else {
-						outcount += item["price"];
-						if (!outData.hasOwnProperty(item["tags"])) outData[item["tags"]] = item["price"];
-						else outData[item["tags"]] += item["price"];
-					}
-				});
-				this.outOpts = {
-					legend:{position: 'bottom'},
-					extra:{ring:{ringWidth: 40,linearType:'custom',centerColor:'#FFF'}},
-					title: {name: "总支出"},
-					subtitle: {name: "￥"+outcount}
-				};
-				this.inOpts = {
-					legend:{position: 'bottom'},
-					extra:{ring:{ringWidth: 40,linearType:'custom',centerColor:'#FFF'}},
-					title: {name: "总收入"},
-					subtitle: {name: "￥"+incount}
-				};
-				//console.log(outData);
-				//console.log(inData);
-				this.outData = {"series": [{"data": this.dic2list(outData)}]};
-				this.inData = {"series": [{"data": this.dic2list(inData)}]};
-			},
-			
 			reload() {
-				this.outData = {"series": [{"data": []}]};
-				this.inData = {"series": [{"data": []}]};
 				var a = this;
 				var date = new Date();
-				var last_time;
-				switch (this.timeArray[this.timeIndex]) {
-					case "近一年": last_time = 365 * 24 * 3600; break;
-					case "近一个月": last_time = 30 * 24 * 3600; break;
-					case "近一周": last_time = 7 * 24 * 3600; break;
-					default: console.log("error");
-				}
-				var start_time = new Date(Math.round(date) - last_time * 1000);
-				var sqlWhere = ' Where day >= ' + start_time.format("YYYY-MM-DD");
-				console.log('select * from database' + sqlWhere);
 				plus.sqlite.selectSql({
 					name: 'moneymap',
-					sql: 'select * from database' + sqlWhere,
+					sql: 'select * from database',
 					success: function(e) {
 						a.sql_data = e;
-						a.updateChart();
+						console.log(a.sql_data)
 						uni.stopPullDownRefresh();
 					},
 					fail: function(e) {
@@ -199,21 +135,8 @@
 						uni.stopPullDownRefresh();
 					}
 				});
-				
+
 			},
-			
-			outDetail () {
-				uni.navigateTo({
-					url: "detail?type=out&time_range=" + this.timeArray[this.timeIndex] + "&sql_data=" + JSON.stringify(this.sql_data)
-				});
-			},
-			
-			inDetail () {
-				uni.navigateTo({
-					url: "detail?type=in&time_range=" + this.timeArray[this.timeIndex] + "&sql_data=" + JSON.stringify(this.sql_data)
-				});
-			},
-			
 		}
 	};
 </script>
@@ -223,12 +146,12 @@
 		margin: 20rpx 0;
 		padding: 0;
 	}
-	
+
 	.charts-box {
-	  width: 100%;
-	  height: 300px;
+		width: 100%;
+		height: 300px;
 	}
-	
+
 	.whatever-some-font {
 		font-family: "STKaiti";
 		font-size: 35rpx;
